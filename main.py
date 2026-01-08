154b0bb9cda3931fdfa33202b7ee3a8ddb3ebe57
flohms18 <trouilletflorent@outlook.com>
2026-01-08 14:52:12 +0100

    Add central service

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI,HTTPException
+import httpx
+from registry import DATA_PRODUCTS, list_data_products, get_product_url
+
+app = FastAPI(title="SpinMesh")
+
+app.get('/products')
+def products():
+    return {
+        'products': list_data_products
+    }
+
+def fetch_product(product_name: str):
+    url = get_product_url(product_name)
+    if not url:
+        raise HTTPException(status_code=404, detail="Data Product not found")
+    try:
+        response = httpx.get(url,timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except httpx.HTTPError as e:
+        raise HTTPException(status_code=500, detail=str(e))
