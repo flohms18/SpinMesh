771460899a82bfb2c87ebb8423250ca54aea7d6e
flohms18 <trouilletflorent@outlook.com>
2026-01-08 15:50:28 +0100

    SpinMesh Done

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 import httpx
-from registry import DATA_PRODUCTS, list_data_products, get_product_url
+from registry import list_data_products, get_product_url
 
 app = FastAPI(title="SpinMesh")
 
-app.get('/products')
-def products():
-    return {
-        'products': list_data_products
-    }
 
+@app.get("/products")
+def products():
+    return {"products": list_data_products()}
+
+
+@app.get("/products/{product_name}")
 def fetch_product(product_name: str):
     url = get_product_url(product_name)
     if not url:
         raise HTTPException(status_code=404, detail="Data Product not found")
+
     try:
-        response = httpx.get(url,timeout=5)
+        response = httpx.get(url, timeout=5)
         response.raise_for_status()
         return response.json()
     except httpx.HTTPError as e:
--- registry.py
+++ registry.py
@@ -1,9 +1,9 @@
 DATA_PRODUCTS = {
-    "rackets" : 'http://127.0.0.1:8000/rackets'
+    "rackets" : 'http://127.0.0.1:8001/rackets'
 }
 
 def list_data_products():
-    return list(DATA_PRODUCTS.key())
+    return list(DATA_PRODUCTS.keys())
 
 def get_product_url(name):
     return DATA_PRODUCTS.get(name)
